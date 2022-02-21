291c094aecb46a6336c168f018eaf5826a699bb1
Okoli Akachukwu <93165724+CodeDeveloper19@users.noreply.github.com>
2022-02-22 00:59:15 +0100

    Add files via upload

--- Memory Card Game/introduction.js
+++ Memory Card Game/introduction.js
@@ -1,9 +1,7 @@
-var mute = document.getElementById("mute");
-var unmute = document.getElementById("unmute");
-var LIMIT = 1;
+var LIMIT = 1; // Allows the program to start playing the audio only once
+var audio = new Audio('backgroundmusic.mp3');
 
 function sound() {
-    var audio = new Audio('backgroundmusic.mp3');
     
     if (LIMIT == 1){
         audio.play();
@@ -11,5 +9,12 @@ function sound() {
         LIMIT += 1;
     }
 
+    if (icon.classList[1] == "fa-volume-xmark") {
+        icon.classList.replace("fa-volume-xmark", "fa-volume-high")
+        audio.muted = false;
+    } else {
+        icon.classList.replace("fa-volume-high", "fa-volume-xmark")
+        audio.muted = true;
+    }
 }
 
